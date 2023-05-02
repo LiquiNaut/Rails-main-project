7aae4e5708d4a73a6339df33cbf0dae95d0c70c8
LiquiNaut <boetfly@gmail.com>
2023-05-02 23:50:11 +0200

    upravene routy pre faktury a pouzivatelov aby sa predoslo nevyziadanym akciam

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :invoices
+  resources :invoices, except: [:index]
   devise_for :users
+  resources :users, except: [:edit, :update, :destroy]
   root "home#index"
 end
