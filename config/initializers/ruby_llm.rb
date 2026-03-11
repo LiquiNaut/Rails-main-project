e3d0bde282cf04c086b78958cefda4a759540581
Boris Gasparovic <boris.gasparovic@iteria.io>
2026-03-11 21:50:09 +0100

    Konfigurácia: prepnutie z OpenAI na OpenRouter provider, use_new_acts_as pre ruby_llm

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -2,5 +2,6 @@ require 'ruby_llm'
 require 'dotenv/load'
 
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV['OPENAI_API_KEY']
+  # config.openai_api_key = ENV['OPENAI_API_KEY']
+  config.openrouter_api_key = ENV['OPENROUTER_API_KEY']
 end
