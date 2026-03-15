d9cbd66e44f16be089f9c77fbd63602f578d2e54
Boris Gasparovic <boris.gasparovic@iteria.io>
2026-03-15 21:35:41 +0100

    Update importmap configuration

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin 'chart.js', to: 'https://esm.sh/chart.js@4.4.7'
+pin '@kurkle/color', to: 'https://esm.sh/@kurkle/color@0.3.2'
